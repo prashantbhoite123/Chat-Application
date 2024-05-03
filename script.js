08e0f34ba883e8b6bd481282fa70babc86ad03d1
prashantbhoite123 <pbhoite985@gmail.com>
2024-05-03 15:22:14 +0530

    project comp

--- script.js
+++ script.js
@@ -4,20 +4,38 @@ AOS.init({
 
 const input = document.querySelector("#input");
 const form = document.querySelector("form");
-console.log(form);
 const sendBtn = document.querySelector("#send-btn");
 const angleChat = document.querySelector(".text-chat-1");
 const prasuChat = document.querySelector(".text-chat-2");
 const prashuSend = document.querySelector(".send-chat-1");
-console.log(prashuSend);
-console.log(angleChat);
-console.log(sendBtn);
-console.log(input);
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
-  console.log("form submit");
-  console.log(input.value);
-  angleChat.innerHTML += ` <p class="accept-chat">${input.value}</p>`;
-  prasuChat.innerHTML += `<p class="accept-prahu">${input.value}</p>`;
+  if (input.value.trim() === "") {
+    alert("send chat");
+  } else {
+    angleChat.innerHTML += ` <p class="accept-chat">${input.value}</p>`;
+    prasuChat.innerHTML += `<p class="accept-prahu">${input.value}</p>`;
+    input.value = "";
+  }
+});
+
+const prahuInput = document.querySelector("#parshu-chat");
+const formTwo = document.querySelector("#fromTwo");
+const sendParshu = document.querySelector("#send-prahu");
+const userAcceptdata = document.querySelector(".userAccepat-data");
+const userDataSend = document.querySelector(".userDatasend");
+console.log(userDataSend);
+console.log(formTwo);
+console.log(prahuInput);
+
+formTwo.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (prahuInput.value.trim() === "") {
+    alert("send chat");
+  } else {
+    angleChat.innerHTML += ` <p class="userAccepat-data">${prahuInput.value}</p>`;
+    prasuChat.innerHTML += `  <p class="userDatasend">${prahuInput.value}</p>`;
+    prahuInput.value = "";
+  }
 });
