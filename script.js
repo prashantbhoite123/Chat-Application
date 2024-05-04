2f7d7d28e9a64849e7133ca6ce06d3bdab0b86ba
prashantbhoite123 <pbhoite985@gmail.com>
2024-05-04 15:55:25 +0530

    project miner changes

--- script.js
+++ script.js
@@ -12,7 +12,9 @@ const prashuSend = document.querySelector(".send-chat-1");
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   if (input.value.trim() === "") {
-    alert("send chat");
+    let speech = new SpeechSynthesisUtterance();
+    speech.text = "Chalaajaa Bhosadeekeee";
+    window.speechSynthesis.speak(speech);
   } else {
     angleChat.innerHTML += ` <p class="accept-chat">${input.value}</p>`;
     prasuChat.innerHTML += `<p class="accept-prahu">${input.value}</p>`;
@@ -25,17 +27,40 @@ const formTwo = document.querySelector("#fromTwo");
 const sendParshu = document.querySelector("#send-prahu");
 const userAcceptdata = document.querySelector(".userAccepat-data");
 const userDataSend = document.querySelector(".userDatasend");
-console.log(userDataSend);
-console.log(formTwo);
-console.log(prahuInput);
+const clearBtn = document.querySelector("#clearBtn");
+console.log(clearBtn);
 
 formTwo.addEventListener("submit", (e) => {
   e.preventDefault();
   if (prahuInput.value.trim() === "") {
-    alert("send chat");
+    let speech = new SpeechSynthesisUtterance();
+    speech.text = "Chalaajaa Bhosadeekeee";
+    window.speechSynthesis.speak(speech);
   } else {
     angleChat.innerHTML += ` <p class="userAccepat-data">${prahuInput.value}</p>`;
     prasuChat.innerHTML += `  <p class="userDatasend">${prahuInput.value}</p>`;
     prahuInput.value = "";
   }
+});
+
+const vishalBtn = document.querySelector("#vishalBtn");
+const masaBtn = document.querySelector("#masaBtn");
+const moreInformation = document.querySelector(".moreinformation");
+
+const moreInformation2 = document.querySelector(".moreinformationTwo");
+const body = document.body;
+
+vishalBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  moreInformation.classList.add("hide");
+});
+
+masaBtn.addEventListener("click", (e) => {
+  e.stopPropagation();
+  moreInformation2.classList.add("hide");
+});
+
+body.addEventListener("click", (e) => {
+  moreInformation2.classList.remove("hide");
+  moreInformation.classList.remove("hide");
 });
